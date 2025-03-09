b8546814d89bf5017ff7d59373aa0b799ab002ad
Hiran <hiranneels03@gmail.com>
2025-03-09 16:48:56 +0530

    Bug fixes

--- model.py
+++ model.py
@@ -17,17 +17,17 @@ class Model:
         """Initialize weights based on weight_init strategy."""
         self.weights = []
         self.neuron_outputs = [np.zeros((input_size,1))]
-        self.dw = []
+        self.dw = [] 
         self.error = [np.zeros((input_size,1))]
         if self.weight_init == 'random':
             prev_layer = input_size
             for i in range(self.num_hidden_layers):
-                self.weights.append(np.random.rand(prev_layer, self.hidden_layer_size[i]))  # Uniform distribution from zero to one
+                self.weights.append(np.random.randn(prev_layer, self.hidden_layer_size[i])*0.01)  
                 self.neuron_outputs.append(np.zeros((self.hidden_layer_size[i],1)))
                 self.error.append(np.zeros((self.hidden_layer_size[i],1)))
                 self.dw.append(np.zeros_like(self.weights[-1]))
                 prev_layer = self.hidden_layer_size[i]
-            self.weights.append(np.random.rand(prev_layer, output_size))
+            self.weights.append(np.random.randn(prev_layer, output_size)*0.01)
             self.neuron_outputs.append(np.zeros((output_size,1)))
             self.error.append(np.zeros((output_size,1)))
             self.dw.append(np.zeros_like(self.weights[-1]))
@@ -48,7 +48,7 @@ class Model:
         if self.activation == 'sigmoid':
             return 1 / (1 + np.exp(-x))
         if self.activation == 'tanh':
-            return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x)) 
+            return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
         if self.activation == 'relu':
             return np.maximum(0,x)
 
@@ -70,7 +70,8 @@ class Model:
             self.neuron_outputs[i + 1] = self.activation_function(np.dot(self.weights[i].T, self.neuron_outputs[i]) + self.biases[i])
         """Forward pass through the network."""
         self.neuron_outputs[-1] = self.activation_function(np.dot(self.weights[-1].T, self.neuron_outputs[-2]) + self.biases[-1])
-        self.neuron_outputs[-1] = np.exp(self.neuron_outputs[-1]) / np.sum(np.exp(self.neuron_outputs[-1])) # Softmax
+        exps = np.exp(self.neuron_outputs[-1] - np.max(self.neuron_outputs[-1]))  # Subtract max for stability
+        self.neuron_outputs[-1] = exps / np.sum(exps)  # Normalize
 
 
     def backward(self, true_output):
@@ -103,6 +104,7 @@ class Model:
         X = np.array([x.reshape(-1, 1) for x in X])
         y = np.array([each_y.reshape(-1, 1) for each_y in y])
         self.initialize_weights(X[0].shape[0], y.shape[1])
+        
         self.initialize_biases(y.shape[1])
         print("Weight matrices")
         for i in range(len(self.weights)) :
@@ -133,17 +135,18 @@ class Model:
                 y_batch = y[start_index: start_index + batch_size]
                 dw = [np.zeros_like(w) for w in self.weights]  # Create zero arrays matching the shape of gradients
                 db = [np.zeros_like(b) for b in self.biases]
-
+                
                 for X_i, y_i in zip(X_batch, y_batch):  
                     self.forward(X_i)
                     self.backward(y_i)
+                    
                     dW_curr, dB_curr = self.gradients()  # Get gradients
 
                     # Accumulate gradients for each layer
                     for i in range(len(dw)):
                         dw[i] += dW_curr[i]
                         db[i] += dB_curr[i]
-
+                
                 self.update_weights(dw, db)  # Update model parameters
                 
                 
@@ -157,8 +160,8 @@ class Model:
                         correct_predictions += 1  # Increment the correct prediction count
                 self.training_loss.append(correct_predictions/batch_size)
                 print(f"Epoch: {epoch + 1}, batch: {int(start_index/batch_size)}/{num_batches} completed....Accuracy: {correct_predictions/batch_size}")
-                                
-
+                
+            
     def predict(self, X):
         """Make predictions on new data."""
         self.forward(X)
